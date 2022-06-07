b762aa75a68dc27f7237f96c73d677dee9b6c16a
sivelswhy <82051273+sivelswhy@users.noreply.github.com>
2022-06-07 21:17:03 +0200

    Missing Code for the copy button to work

--- script.js
+++ script.js
@@ -7,18 +7,25 @@ gtag('config', 'G-3S62TTDK8Q');
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTo = 0;
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTo = 0;
+// }
+
+function copyEvent(id) {
+  var str = document.getElementById(id)
+  window.getSelection().selectAllChildren(str)
+  document.execCommand("Copy")
+  window.alert("J'ai copié " + id)
 }
